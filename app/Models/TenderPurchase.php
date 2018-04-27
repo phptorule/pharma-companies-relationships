3536049ca08b6cad1db9e8c34ffa64427461531d
driller <driller@outlook.com>
2018-04-27 16:48:24 +0300

    add product controller

--- app/Models/TenderPurchase.php
+++ app/Models/TenderPurchase.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class TenderPurchase extends Model
 {
 	protected $table = 'rl_address_tenders_purchase';
+
+	public function tender()
+	{
+		return $this->belongsTo(Tender::class);
+	}
 }
