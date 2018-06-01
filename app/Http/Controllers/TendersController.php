d1bb310440dc9ad1acf5252983a63ec7d0f3f4ab
driller <driller@outlook.com>
2018-06-01 18:00:42 +0300

    Create tenders function

--- app/Http/Controllers/TendersController.php
+++ app/Http/Controllers/TendersController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-
 use App\Models\Tender;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class TendersController extends Controller {
-	function getTendersByAddress( $id ) {
+
+	public function getTendersByAddress( $id ) {
 
         $actualDate = date( "Y" );
 
@@ -61,4 +61,107 @@ class TendersController extends Controller {
 			'rateNextYear'     => $rateNextYear,
 		] );
 	}
+
+	public function getTendersByProductAndAdderssPaginated($product, $address) {
+
+		$query = $this->prepareTendersQuery($address, $product);
+
+		$tenders = $query->paginate( 5 );
+
+		return response()->json( $tenders );
+
+	}
+
+	public function getTendersByAdderssPaginated($address) {
+
+		$query = $this->prepareTendersQuery($address);
+
+		$tenders = $query->paginate( 5 );
+
+		return response()->json( $tenders );
+	}
+
+	public function getTendersByProductAndAddressToExcel($product, $address) {
+
+		$query = $this->prepareTendersQuery($address, $product);
+
+		$tenders = $query->get();
+
+		return response()->json( $tenders );
+	}
+
+	public function getTendersByAddressToExcel($address) {
+
+		$query = $this->prepareTendersQuery($address);
+
+		$tenders = $query->get();
+
+		return response()->json( $tenders );
+	}
+
+	public function prepareTendersQuery($address, $id = '') {
+
+		$select = 'at.id as tender_id, at.address_id, at.budgeted_cost, at.actual_cost, at.url as tender_url, at.tender_date,
+					atb.budget,
+					atp.id as purchase_id, atp.quantity as purchase_quantity, atp.total_price as purchase_total_price,
+					atp.name as purchase_name,atp.remark as purchase_remark,
+					pc.name as tag_name, pc.id as tag_id,
+					ats.amount as suppliers_amount,
+					s.name as suppliers_name, s.address as suppliers_address';
+
+		$query = DB::table( 'rl_address_tenders AS at' )
+		           ->select( DB::raw( $select ) )
+		           ->where( 'p.id', $id )
+		           ->where( 'at.address_id', $address )
+		           ->whereNotNull( 'atp.tender_id' )
+		           ->whereNotNull( 'atb.tender_id' )
+		           ->leftJoin( 'rl_address_tenders_purchase AS atp', 'atp.tender_id', '=', 'at.id' )
+		           ->leftJoin( 'rl_address_tenders_budgets AS atb', 'atb.tender_id', '=', 'at.id' )
+		           ->leftJoin( 'rl_address_tenders_purchase_products AS atpp', 'atpp.purchase_id', '=', 'atp.id' )
+		           ->leftJoin( 'rl_product_consumables AS pc', 'atpp.consumable_id', '=', 'pc.id' )
+		           ->leftJoin( 'rl_address_tenders_suppliers AS ats', 'ats.tender_id', '=', 'at.id' )
+		           ->leftJoin( 'rl_suppliers AS s', 's.id', '=', 'ats.supplier_id' )
+		           ->leftJoin( 'rl_products AS p', 'p.id', '=', 'atpp.product_id' );
+
+		$query = $this->composeConditions( $query, request()->all() );
+
+		return $query;
+	}
+
+	public function composeConditions( $query, $requestParams ) {
+
+		if ( isset( $requestParams['sort-by'] ) ) {
+
+			$field     = explode( '-', $requestParams['sort-by'] )[0];
+			$direction = explode( '-', $requestParams['sort-by'] )[1];
+
+			if ( $field == 'budget' ) {
+				$field = 'budgeted_cost';
+			} else if ( $field == 'date' ) {
+				$field = 'tender_date';
+			} else if ( $field == 'tenders' ) {
+				$field = 'at.id';
+			}
+
+			$query->orderBy( $field, $direction );
+		}
+
+		if ( isset( $requestParams['min'] ) && isset( $requestParams['max'] ) ) {
+			$query->where( 'budgeted_cost', '>=', $requestParams['min'] )
+			      ->where( function ( $q ) use ( $requestParams ) {
+				      $q->where( 'budgeted_cost', '<=', $requestParams['max'] );
+			      } );
+		}
+
+		if ( isset( $requestParams['tenders-search'] ) ) {
+			$query->where( 'atp.name', 'LIKE', '%' . $requestParams['tenders-search'] . '%' );
+		}
+
+		if ( isset( $requestParams['tag-cons'] ) ) {
+			$query->whereIn( 'consumable_id', $requestParams['tag-cons'] );
+		}
+
+		return $query;
+	}
+
 }
