b30c4d7528860c86daf6c13dd1a3c2996279f0a2
sergezak <sergezak9@gmail.com>
2018-06-04 11:05:46 +0300

    sort people by sum(edgeweight) of shared connections in person relationship #42

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -36,7 +36,7 @@ class PeopleController extends Controller
     function getPersonRelationships(People $person)
     {
         $relationships = DB::table('rl_address_connections AS rl1')
-            ->select(DB::raw("from_person_id, to_person_id, MAX(edge_weight) as edge_weight,
+            ->select(DB::raw("from_person_id, to_person_id, SUM(edge_weight) as edge_weight,
             (SELECT a1.edge_comment FROM rl_address_connections as a1 WHERE from_person_id = $person->id AND rl1.to_person_id = to_person_id AND a1.edge_type = '1') as co_authored_paper,
             (SELECT a2.edge_comment FROM rl_address_connections as a2 WHERE from_person_id = $person->id AND rl1.to_person_id = to_person_id AND a2.edge_type = '2') as cited_paper,
             (SELECT a3.edge_comment FROM rl_address_connections as a3 WHERE from_person_id = $person->id AND rl1.to_person_id = to_person_id AND a3.edge_type = '3') as signatory_at_company,
