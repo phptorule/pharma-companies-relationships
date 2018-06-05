403a35377fd41709d2e0555502db63877ef7589d
bisend <vboychenko54@gmail.com>
2018-06-05 10:29:25 +0300

    #161 Fix - add search for person "all employees" sidebar

--- resources/assets/js/private-components/pages/address-details.vue
+++ resources/assets/js/private-components/pages/address-details.vue
@@ -707,10 +707,10 @@
         },
 
         mounted: function () {
-            let iOS = /Mac|iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-            let ih = (iOS) ? screen.height : window.innerHeight;
-            $('.address-details-fixed-height').height(ih - 70 - 51 - 13);
-            $('.slided-box').height(ih - 70 - 51 - 13);
+            // let iOS = /Mac|iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+            // let ih = (iOS) ? screen.height : window.innerHeight;
+            // $('.address-details-fixed-height').height(ih - 70 - 51 - 13);
+            // $('.slided-box').height(ih - 70 - 51 - 13);
 
             this.addressId = this.$route.params.id;
 
