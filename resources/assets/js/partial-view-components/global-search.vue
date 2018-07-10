b59b5a517a7fc5dc8043b7a426aa46b17283cc69
sergezak <sergezak9@gmail.com>
2018-07-10 17:35:49 +0300

    navigate to appropriate page, when global search is performed  #215

--- resources/assets/js/partial-view-components/global-search.vue
+++ resources/assets/js/partial-view-components/global-search.vue
@@ -110,6 +110,15 @@
 
                         this.$eventGlobal.$emit('notifyGlobalSearchCountResults', data);
 
+                        let hash = this.$route.hash;
+
+                        if(+data.count_addresses === 0 && +data.count_people > 0) {
+                            this.$router.push('/people-dashboard?people-ids=' + data.people_ids.toString() + hash)
+                        }
+                        else {
+                            this.$router.push('/dashboard?address-ids=' + data.address_ids.toString() + hash)
+                        }
+
                         return data;
                     })
             },
