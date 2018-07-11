bc29b17179ae4bd29c7571691dd41515eee07737
sergezak <sergezak9@gmail.com>
2018-07-11 15:09:36 +0300

    inited GlobalSearchService #215

---
+++ app/Services/GlobalSearchService.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: developer
+ * Date: 11.07.18
+ * Time: 15:05
+ */
+
+namespace App\Services;
+
+
+use Illuminate\Support\Facades\Log;
+
+class GlobalSearchService
+{
+
+    function __construct()
+    {
+        Log::info('it is working');
+    }
+
+}
