3f39ec34ebceef245ca4034aa5720b7c7f2ec463
sergezak <sergezak9@gmail.com>
2018-07-26 15:27:18 +0300

    set styles for feed search input #261

--- resources/assets/js/partial-view-components/feed-sidebar.vue
+++ resources/assets/js/partial-view-components/feed-sidebar.vue
@@ -1,6 +1,84 @@
 <template>
     <div class="feed-sidebar">
 
+        <div class="search-input-container">
+
+            <div class="search-input-box">
+                <i class="fa fa-search"></i>
+
+                <input type="text" class="search-input" placeholder="Free text search">
+            </div>
+
+        </div>
+
+        <div class="feed-sidebar-tabs">
+            <ul>
+                <li>
+                    <a href="javascript:void(0)">
+                        All
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)">
+                        Saved
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="filter-container">
+            <div class="box box-default">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Sort by</h3>
+
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                    </div>
+                    <!-- /.box-tools -->
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    The body of the box
+                </div>
+                <!-- /.box-body -->
+            </div>
+
+            <div class="box box-default">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Type</h3>
+
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                    </div>
+                    <!-- /.box-tools -->
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    The body of the box
+                </div>
+                <!-- /.box-body -->
+            </div>
+
+            <div class="box box-default">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Range</h3>
+
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                    </div>
+                    <!-- /.box-tools -->
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    The body of the box
+                </div>
+                <!-- /.box-body -->
+            </div>
+        </div>
+
     </div>
 </template>
 
