9dc17fcd1495b860594b6392df3fe0636116cd42
sergezak <sergezak9@gmail.com>
2018-07-31 11:13:43 +0300

    implemented loggin changes for Address model

--- app/Models/UserEdit.php
+++ app/Models/UserEdit.php
@@ -4,35 +4,49 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
 class UserEdit extends Model
 {
 
-
     protected $table = 'rl_user_edits';
+
+    protected $fillable = ['user_id', 'entity_id', 'entity_row_key', 'field', 'new_value', 'old_value', 'url_formatter', 'batch_uuid'];
+
 
     static function UUID ()
     {
         return md5(Carbon::now());
     }
 
+
     static function log(Model $model)
     {
-
         $oldState = $model->getOriginal();
 
         $newState = json_decode($model->toJSON());
 
-        Log::info('oldState ---> ' . print_r($oldState, 1));
-
-        Log::info('newState ' . print_r($newState, 1));
+        $uuid = self::UUID();
 
         foreach ($newState as $prop => $value) {
 
             if($newState->$prop != $oldState[$prop]) {
                 Log::info("OLD value of $prop: " . $oldState[$prop]);
                 Log::info("NEW value of $prop: " . $newState->$prop);
+
+                $params = [
+                    'user_id' => Auth::user()->id,
+                    'entity_id' => Entity::where('table_name', $model->getTable())->first()->id,
+                    'entity_row_key' => $model->id,
+                    'field' => $prop,
+                    'new_value' => $newState->$prop,
+                    'old_value' => $oldState[$prop],
+                    'url_formatter' => null,
+                    'batch_uuid' => $uuid
+                ];
+
+                self::create($params);
             }
 
         }
